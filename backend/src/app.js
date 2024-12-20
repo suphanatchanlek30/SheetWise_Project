ef4b0e43128390f8e37ce31dc0b920b439304f4a
suphanatchanlek <suphanatchanlek@gmail.com>
2024-12-20 19:47:17 +0700

    add swagger for api

--- backend/src/app.js
+++ backend/src/app.js
@@ -2,6 +2,7 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const cors = require('cors');
 const dotenv = require('dotenv');
+
 const authRoutes = require('./routes/authRoutes');
 const sheetRoutes = require('./routes/sheetRoutes');
 const orderRoutes = require('./routes/orderRoutes');
@@ -11,6 +12,10 @@ const adminRoutes = require('./routes/adminRoutes');
 const fileRoutes = require('./routes/fileRoutes');
 const paymentRoutes = require('./routes/paymentRoutes');
 const analyticsRoutes = require('./routes/analyticsRoutes');
+
+const swaggerUi = require('swagger-ui-express');
+const YAML = require('yamljs');
+const swaggerDocument = YAML.load('./swagger.yaml');
 
 // โหลดตัวแปรแวดล้อมจาก .env
 dotenv.config();
@@ -31,6 +36,7 @@ app.use('/api/admin', adminRoutes);
 app.use('/api/files', fileRoutes);
 app.use('/api/payments', paymentRoutes);
 app.use('/api/analytics', analyticsRoutes);
+app.use('/api/docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 
 // เส้นทางพื้นฐาน
 app.get('/', (req, res) => {
